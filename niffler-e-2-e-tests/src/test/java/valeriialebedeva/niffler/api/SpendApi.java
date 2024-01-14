0c17f0a538dff9272be0ec71dcf30fb490d6690e
ValeriiaLebedeva <62835399+ValeriiaLebedeva@users.noreply.github.com>
2024-01-14 04:06:14 +0100

    added hw1

---
+++ niffler-e-2-e-tests/src/test/java/valeriialebedeva/niffler/api/SpendApi.java
@@ -0,0 +1,12 @@
+package valeriialebedeva.niffler.api;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import valeriialebedeva.niffler.model.SpendJson;
+
+public interface SpendApi {
+
+  @POST("/addSpend")
+  Call<SpendJson> addSpend(@Body SpendJson spend);
+}
---
+++ niffler-e-2-e-tests/src/test/java/valeriialebedeva/niffler/test/SpendingTest.java
@@ -0,0 +1,26 @@
+package valeriialebedeva.niffler.test;
+
+import org.junit.jupiter.api.Test;
+import valeriialebedeva.niffler.jupiter.GenerateSpend;
+import valeriialebedeva.niffler.model.CurrencyValues;
+import valeriialebedeva.niffler.model.SpendJson;
+
+
+public class SpendingTest extends TestBase{
+
+  @GenerateSpend(
+      username = "duck",
+      description = "QA.GURU Advanced 4",
+      amount = 72500.00,
+      category = "Обучение",
+      currency = CurrencyValues.RUB
+  )
+
+  @Test
+  void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend) {
+
+    mainPage.findSpendingByDescription(spend.description())
+                    .deleteSpending()
+            .checkTableHasNoSpending();
+  }
+}
